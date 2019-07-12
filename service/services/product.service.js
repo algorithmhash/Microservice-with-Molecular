1a48f61a6f1a4109861be305bcc868457cf59417
Hunter Rafuse <hunter@Hunter-Rafuse.local>
2019-07-12 15:17:54 +0300

    firs initial commit, for service

---
+++ service/services/product.service.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const DbService = require("moleculer-db");
+const MongoAdapter = require("moleculer-db-adapter-mongo");
+
+"use strict";
+
+const DbService = require("moleculer-db");
+const MongoAdapter = require("moleculer-db-adapter-mongo");
+
+module.exports = {
+    name: "products",
+    mixins: [DbService],
+    adapter: new MongoAdapter("mongodb://localhost/service"),
+    collection: "products"
+};
